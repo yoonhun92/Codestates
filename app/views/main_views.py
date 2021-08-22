b99ef8fd98d38e39baf3aac3596b72c185917dd3
yoonhun92 <80502202+yoonhun92@users.noreply.github.com>
2021-08-23 01:13:38 +0900

    git

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -2,19 +2,19 @@ from flask import Blueprint, render_template, url_for
 from werkzeug.utils import redirect
 from app.models import Question
 
-bp = Blueprint('main', __name__, url_prefix='/') # 
+bp = Blueprint('main', __name__, url_prefix='/') # blueprint 선언, 이름, 모듈명, URL 프리픽스 값 
 
 
-@bp.route('/hello')
+@bp.route('/hello') # 
 def hello_pybo():
         return 'Hello, Pybo!'
 
 
 @bp.route('/')
 def index():
-    return redirect(url_for('question._list'))
-        #question_list = Question.query.order_by(Question.create_date.desc())
-        #return render_template('question/question_list.html', question_list=question_list)
+    return redirect(url_for('question._list')) # url_for 기능으로 리다이렉트
+        #question_list = Question.query.order_by(Question.create_date.desc()) # 조회 결과 정렬 함수
+        #return render_template('question/question_list.html', question_list=question_list) #
 
 #@bp.route('/detail/<int:question_id>/')
 #def detail(question_id):
